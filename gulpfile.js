9ed18905c10a68f76b7c78544ef622c72f1f45b5
przemekd <przemekd@sparkbit.pl>
2018-02-05 17:23:57 +0100

    SPARKSON-2: make sourcemaps generations optional
    Change-Id: I4acb116894be4ab01d39eb729e1875cd99bd0b69

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ const gulp = require("gulp");
 const path = require("path");
 const runSequence = require("run-sequence");
 const sourcemaps = require("gulp-sourcemaps");
+const gulpif = require('gulp-if');
 const ts = require("gulp-typescript");
-
+const argv = require('yargs').argv;
 
 gulp.task("clean", () => {
     return del(["lib", "dts"]);
@@ -13,16 +14,18 @@ gulp.task("clean", () => {
 const tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("transpile-ts", () => {
+    var withSourceMaps = argv.withSourceMaps;
+
     const tsResult = tsProject.src()
-        .pipe(sourcemaps.init())
+        .pipe(gulpif(withSourceMaps, sourcemaps.init()))
         .pipe(tsProject());
     return tsResult.js
-        .pipe(sourcemaps.write({
+        .pipe(gulpif(withSourceMaps, sourcemaps.write({
             includeContent: true,
             sourceRoot: function (file) {
                 return path.relative(path.dirname(file.path), file.base);
             }
-        }))
+        })))
         .pipe(gulp.dest("lib"));
 });
 
